979dd67b584047a744e1881f265944c9f920ffad
rpamaong <rpamaong@startechup.com>
2022-04-18 23:39:41 +0800

    feat: initial card stories

---
+++ src/components/Card/Card.stories.jsx
@@ -0,0 +1,14 @@
+import Card from "./index";
+
+export default {
+  title: "Components/Card",
+  component: Card,
+};
+
+export const Component = (args) => <Card {...args} />;
+
+//todo: card arguments...
+// width,image,padding,border
+
+// how to use another external component here?
+// example: Button inside the Card component...
